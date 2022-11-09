b0cf9282129e6bbdffad104923beff6b489ac2e1
OguzhanCiftci75 <lenovo918918@hotmail.com>
2022-11-10 01:57:42 +0400

    Add project files.

---
+++ Ders4GenericsIntro/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ders4GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;        // Metod dışında yazılan bir değişken ama yine metod içinden erişilebilir 
+
+        //Constructor yapıcı metod
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; // aşşağıdaki referans numarasını başkasına tuttururuz
+            items = new T[items.Length + 1]; // elemanları +1 şekilde arttırırız
+           
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item; // artık ekleme işlemi yapılabilir 
+
+        }
+    }
+}
---
+++ Ders4GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ders4GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Oğuzhan");
+            
+        }
+    }
+}
